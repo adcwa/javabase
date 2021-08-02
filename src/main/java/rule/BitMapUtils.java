8387b36d20755da197604e54669cbb7999901b4c
wangfeng <wangf@gantsoftware.com>
2021-08-02 08:21:22 +0800

    npr

--- src/main/java/rule/BitMapUtils.java
+++ src/main/java/rule/BitMapUtils.java
@@ -1,14 +1,104 @@
 package rule;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * bitmap 工具
+ * 1、 规则解析
+ * 2、 规则推导
+ * 3、 结果输出
+ * 4、 族下唯一？？？
  * @author 54117
  */
 public class BitMapUtils {
-
+    /**
+     * 设置bitmap  ，将num对应位置的bit设置为1
+     * @param bits
+     * @param num
+     */
     public  static  void setBit(byte[] bits, int num){
         bits[num>>3] |=  1 << num%8;
     }
-    public  static  int getBit(byte[] bits, int num){
-        return bits[num>>3] &  (1<<num%8);
+
+    public  static  void setBitn(byte[] bits, int num){
+        bits[num>>3] |=  1 << num%8;
     }
+
+    /**
+     * 判断是否存在，存在则返回 1
+     * @param bits
+     * @param num
+     * @return
+     */
+    public  static  int exist(byte[] bits, int num){
+        return (bits[num>>3] &  (1<<num%8)) >> num%8;
+    }
+
+    /**
+     * 匹配两个bitmap， 返回cmp与base不匹配的数据，并推导出原数据下标位置
+     * @param byteBase
+     * @param byteCmp
+     * @return  不匹配数据的原值
+     */
+    public static List<Integer> match(byte[] byteBase, byte[] byteCmp){
+        //将两个bitmap 进行比较
+        byte[] a = and(byteBase,byteCmp);
+        byte[] x = xor(a,byteCmp);
+
+        return deduceIdx(x);
+    }
+
+    /**
+     * 推导原值
+     * @param bytes bitmap
+     * @return 顺序输出的原值
+     */
+    public static List<Integer> deduceIdx(byte[] bytes){
+//        int[] idx = new int[bytes.length*8];
+        List<Integer> idx = new ArrayList<>();
+
+        for (int i = 0; i < bytes.length; i++) {
+            if(bytes[i] != 0){
+                int v = i*8;
+                for (int offset = 0; offset < 8; offset++) {
+                    int  b = 1<<offset;
+                    if((bytes[i] &b) == b){
+                        idx.add(v+offset);
+                    }
+                }
+            }
+        }
+
+        return idx;
+    }
+
+    /**
+     * and
+     * @param byteBase
+     * @param byteCmp
+     * @return
+     */
+    public static byte[] and(byte[] byteBase, byte[] byteCmp){
+        byte[] ret = new byte[byteCmp.length];
+        for (int i = 0; i < byteBase.length; i++) {
+            ret[i] = (byte) (byteBase[i] & byteCmp[i]);
+        }
+        return ret ;
+    }
+
+    /**
+     * xor
+     * @param byteBase
+     * @param byteCmp
+     * @return
+     */
+    public static byte[] xor(byte[] byteBase, byte[] byteCmp){
+        byte[] ret = new byte[byteCmp.length];
+        for (int i = 0; i < byteBase.length; i++) {
+            ret[i] = (byte) (byteBase[i] ^ byteCmp[i]);
+        }
+        return ret ;
+    }
+
 }
---
+++ src/main/java/rule/NPR.java
@@ -0,0 +1,86 @@
+package rule;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class NPR {
+
+    /**
+     * 将中缀表达式转换为后缀表达式（逆波兰表达式）
+     * @param express
+     * @return
+     */
+    public static String transfer(String express){
+        Stack<String> stack = new Stack<>();
+        List<String> list= new ArrayList<>();
+        for (int i=0;i<express.length();i++){
+            if ((express.charAt(i)+"").matches("\\d")){
+                list.add(express.charAt(i)+"");
+            }else if((express.charAt(i)+"").matches("[\\+\\-\\*\\/]")){
+                //如果stack为空
+                if (stack.isEmpty()){
+                    stack.push(express.charAt(i)+"");
+                    continue;
+                }
+                //不为空
+
+                //上一个元素不为（，且当前运算符优先级小于上一个元素则，将比这个运算符优先级大的元素全部加入到队列中
+                while (!stack.isEmpty()&&!stack.lastElement().equals("(")&&!comparePriority(express.charAt(i)+"",stack.lastElement())){
+                    list.add(stack.pop());
+                }
+                stack.push(express.charAt(i)+"");
+            }else if(express.charAt(i)=='('){
+                //遇到左小括号无条件加入
+                stack.push(express.charAt(i) + "");
+            }else if(express.charAt(i)==')'){
+                //遇到右小括号，则寻找上一堆小括号，然后把中间的值全部放入队列中
+                while(!("(").equals(stack.lastElement())){
+                    list.add(stack.pop());
+                }
+                //上述循环停止，这栈顶元素必为"("
+                stack.pop();
+            }
+        }
+        //将栈中剩余元素加入到队列中
+        while (!stack.isEmpty()){
+            list.add(stack.pop());
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        //变成字符串
+        for (String s : list) {
+            stringBuffer.append(s);
+        }
+        return stringBuffer.toString();
+    }
+
+    /**
+     * 比较运算符的优先级
+     * @param o1
+     * @param o2
+     * @return
+     */
+    public static boolean comparePriority(String o1,String o2){
+        return getPriorityValue(o1)>getPriorityValue(o2);
+    }
+
+    /**
+     * 获得运算符的优先级
+     * @param str
+     * @return
+     */
+    private static int getPriorityValue(String str) {
+        switch(str){
+            case "+":
+                return 1;
+            case "-":
+                return 1;
+            case "*":
+                return 2;
+            case "/":
+                return 2;
+            default:
+                throw new RuntimeException("没有该类型的运算符！");
+        }
+    }
+}
